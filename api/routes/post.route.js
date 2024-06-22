a28cf38eb7e72c0ac6085607093416dde9030751
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2024-06-22 15:27:45 +0900

    add auth, jwttoken, cookie

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,8 @@
 import express from "express";
-
 const router = express.Router();
-
-// router.get("/test", (req, res) => {
-//   console.log("router works!");
-// });
+router.get("/test", (req, res) => {
+  console.log("router works!");
+});
 // router.post("/test", (req, res) => {
 //   console.log("router works!");
 // });
